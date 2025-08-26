e9cf0e705e6bc2a50460afb01d5723586d831627
David Tang <david_tang@web.de>
2025-08-26 13:03:35 +0200

    input-page created and basic bootstrp

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,24 @@
+class JournalsController < ApplicationController
+  def new
+    @journal = Journal.new
+  end
+
+  def create
+
+    @journal = Journal.new(journal_params)
+    # -> ask the AI here
+    @journal.title = RubyLLM.chat.ask("create a 2-5 world title for this journal entry: #{@journal.content}").content
+    @journal.summary = RubyLLM.chat.ask("create a 2 sentence summary for this journal entry: #{@journal.content}").content
+    if @journal.save
+      redirect_to root_path, notice: "Journal created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def journal_params
+    params.require(:journal).permit(:content)
+  end
+end
