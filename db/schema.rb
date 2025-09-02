87f2b87b23854192dd993e58e46ad9d984df6aff
Yvonne Edmonds <yvonne.edmonds1.1@gmail.com>
2025-09-02 16:22:13 +0200

    fixed schema and checked to do update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_01_152441) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_02_135443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -178,7 +178,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_01_152441) do
     t.string "description"
     t.date "due_date"
     t.boolean "status"
-    t.bigint "journal_id", null: false
+    t.bigint "journal_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
