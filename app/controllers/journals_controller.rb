894db136e6b08e98097ddacbad2c3d4ac786e5cf
David Tang <david_tang@web.de>
2025-09-05 14:45:59 +0200

    skip noctice

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -40,7 +40,7 @@ class JournalsController < ApplicationController
     end
 
     if @journal.save
-      redirect_to todos_path(journal_id: @journal.id)
+      redirect_to todos_path(journal_id: @journal.id), notice: nil
     else
       render :new, status: :unprocessable_entity
       return
